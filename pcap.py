3d4a2c6d54f1632f50cc82e90654fb92ee7fe229
whitehat92 <ze_correia93@hotmail.com>
2019-06-05 21:43:35 +0100

    Update pcap.py

--- pcap.py
+++ pcap.py
@@ -1,7 +1,23 @@
-import os
+timeprompt = str(input("Do you want to print with time? (Y/N): "))
+if timeprompt == "Y" or timeprompt == "y" or timeprompt == "":
 
-print("---------pcap analyser------------")
-#os.system()
-
-
-
+    pcap = rdpcap('<name_of_file_here.pcap')
+    s = pcap.sessions()
+    print("------- SUMMARY OF CAPTURE ------")
+    print(pcap.summary)
+    print("------- SESSIONS --------")
+    for a in s.items():
+        print(a)
+    print("-------- TIME ----------")
+    for b, c in s.items():
+        for x in c:
+            print(x.time, b)
+else:
+    pcap = rdpcap('<name_of_file_here.pcap')
+    s = pcap.sessions()
+    print("------- SUMMARY OF CAPTURE ------")
+    print(pcap.summary)
+    print("------- SESSIONS --------")
+    print(s)
+    for a in s.items():
+        print(a)
